36eaf8de4d28ea7900fff79ad2d6237f4bc4f9f1
Kwinten Fruytier <kwinten@thewave.tech>
2022-04-09 19:02:35 +0200

    added changes to services

--- src/pages/services.js
+++ src/pages/services.js
@@ -28,10 +28,10 @@ const Services = () => {
               ></StaticImage>
             </summary>
             <p className={styles.brainfoodTxt}>
-              Brainfood - Wij denken heel graag met je mee. Ben je op zoek naar
-              een sparring partner om jouw idee te pitchen, en het verder te
-              laten evolueren? Of heb je nood aan advies en ondersteuning? Meer
-              weten over adviserende verkoop?
+              <span>Brainfood</span> Wij denken heel graag met je mee. Ben je op
+              zoek naar een sparring partner om jouw idee te pitchen, en het
+              verder te laten evolueren? Of heb je nood aan advies en
+              ondersteuning? Meer weten over adviserende verkoop?
             </p>
           </details>
         </button>
@@ -44,12 +44,23 @@ const Services = () => {
             )
           }}
         >
-          <StaticImage
-            src="../images/journey.png"
-            width={250}
-            className={styles.journeyImage}
-            alt="journey"
-          ></StaticImage>
+          <details>
+            <summary>
+              <StaticImage
+                src="../images/journey.png"
+                width={250}
+                className={styles.journeyImage}
+                alt="journey"
+              ></StaticImage>
+            </summary>
+            <p>
+              <span>Journey</span> Een realiseerbaar traject op maat van je
+              organisatie. Direct implementeerbaar, met begeleiding en
+              opvolging. Hierbij streven we naar voortdurende progressie van het
+              team en van de productiviteit. Knowlegde transfer zal centraal
+              staan."
+            </p>
+          </details>
         </button>
 
         <button
@@ -60,12 +71,23 @@ const Services = () => {
             )
           }}
         >
-          <StaticImage
-            src="../images/project.png"
-            width={250}
-            className={styles.projectImage}
-            alt="project"
-          ></StaticImage>
+          <details>
+            <summary>
+              <StaticImage
+                src="../images/project.png"
+                width={250}
+                className={styles.projectImage}
+                alt="project"
+              ></StaticImage>
+            </summary>
+            <p>
+              <span>Project</span> Op zoek naar ondersteuning voor je projecten?
+              Wij kunnen fungeren als projectleider, of wij kunnen verschillende
+              andere projectrollen opnemen. Tegelijkertijd zorgen wij voor een
+              kennisoverdracht binnen je team zodat toekomstige projecten
+              zelfstandig uitgevoerd kunnen worden.
+            </p>
+          </details>
         </button>
 
         <button
@@ -76,12 +98,20 @@ const Services = () => {
             )
           }}
         >
-          <StaticImage
-            src="../images/speaker.png"
-            width={250}
-            className={styles.speakerImage}
-            alt="speaker"
-          ></StaticImage>
+          <details>
+            <summary>
+              <StaticImage
+                src="../images/speaker.png"
+                width={250}
+                className={styles.speakerImage}
+                alt="speaker"
+              ></StaticImage>
+            </summary>
+            <p>
+              <span> Speaker</span>Op zoek naar een spreker of facilitator voor
+              je evenement of workshop ? Wij helpen je graag verder
+            </p>
+          </details>
         </button>
 
         <button
@@ -92,12 +122,20 @@ const Services = () => {
             )
           }}
         >
-          <StaticImage
-            src="../images/visuals.png"
-            width={250}
-            className={styles.visualsImage}
-            alt="visuals"
-          ></StaticImage>
+          <details>
+            <summary>
+              <StaticImage
+                src="../images/visuals.png"
+                width={250}
+                className={styles.visualsImage}
+                alt="visuals"
+              ></StaticImage>
+            </summary>
+            <p>
+              <span>Visuals</span>Flows, processen of simpelweg de agendapunten
+              van een meeting duidelijk visualiseren, dat is ons ding.
+            </p>
+          </details>
         </button>
       </div>
     </Layout>
