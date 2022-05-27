d4472c639d88dc9f36d2ab4ba5cd936d74d0eac0
Kwinten Fruytier <kwinten@thewave.tech>
2022-05-27 13:56:26 +0200

    optimized website

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -33,8 +33,11 @@ const Contact = () => {
         </div>
         <div className={styles.maps}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2502.9465397477843!2d4.115734415431693!3d51.1463369448595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c38e5852e01197%3A0x5ca621fc9869c390!2sBookmolenstraat%2087%2C%209111%20Sint-Niklaas!5e0!3m2!1sen!2sbe!4v1649069218714!5m2!1sen!2sbe"
-            title="Google Maps Humital home address"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2502.9465397477843!2d4.115734415431693!3d51.1463369448595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c38e5852e01197%3A0x5ca621fc9869c390!2sBookmolenstraat%2087%2C%209111%20Sint-Niklaas!5e0!3m2!1sen!2sbe!4v1653649581975!5m2!1sen!2sbe"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            title="Google Maps Humital Home Address"
           ></iframe>
         </div>
       </div>
--- src/pages/services.js
+++ src/pages/services.js
@@ -10,9 +10,127 @@ const Services = () => {
       <Seo title="Diensten" />
 
       <div className={styles.aboutBanner}>
-        <h1 className={styles.bannerTitle}>Onze diensten</h1>
+        <h1 className={styles.bannerTitle}>Wat kunnen wij voor jou doen?</h1>
+      </div>
+      {/*------------ BRAINFOOD ------------ */}
+      <div className={styles.BrainfoodContainer}>
+        <div className={styles.BrainfoodLeftContainer}>
+          <div className={styles.BrainfoodLeftContent}>
+            <StaticImage
+              src="../images/services/brainfood.png"
+              alt="Brainfood"
+              width={300}
+              className={styles.imageBrainfood}
+            ></StaticImage>
+          </div>
+        </div>
+        <div className={styles.BrainfoodRightContainer}>
+          <div className={styles.BrainfoodRightContent}>
+            <h4 className={styles.serviceName}>Brainfood</h4>
+            <p>
+              Wij denken heel graag met je mee. Ben je op zoek naar een sparring
+              partner om jouw idee te pitchen, en het verder te laten evolueren?
+              Of heb je nood aan advies en ondersteuning? Meer weten over
+              adviserende verkoop?
+            </p>
+          </div>
+        </div>
+      </div>
+      {/*------------ Journey ------------ */}
+      <div className={styles.JourneyContainer}>
+        <div className={styles.JourneyRightContainer}>
+          <div className={styles.JourneyRightContent}>
+            <StaticImage
+              src="../images/services/journey.png"
+              alt="Journey"
+              width={300}
+              className={styles.imageJourney}
+            ></StaticImage>
+          </div>
+        </div>
+        <div className={styles.JourneyLeftContainer}>
+          <div className={styles.JourneyLeftContent}>
+            <h4 className={styles.serviceName}>Journey</h4>
+            <p>
+              Een realiseerbaar traject op maat van je organisatie. Direct
+              implementeerbaar, met begeleiding en opvolging. Hierbij streven we
+              naar voortdurende progressie van het team en van de
+              productiviteit. Knowlegde transfer zal centraal staan.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/*------------ PROJECT ------------ */}
+      <div className={styles.ProjectsContainer}>
+        <div className={styles.ProjectsLeftContainer}>
+          <div className={styles.ProjectsLeftContent}>
+            <StaticImage
+              src="../images/services/project.png"
+              alt="Project"
+              width={300}
+              className={styles.imageProjects}
+            ></StaticImage>
+          </div>
+        </div>
+        <div className={styles.ProjectsRightContainer}>
+          <div className={styles.ProjectsRightContent}>
+            <h4 className={styles.serviceName}>Project</h4>
+            <p>
+              Op zoek naar ondersteuning voor je projecten? Wij kunnen fungeren
+              als projectleider, of wij kunnen verschillende andere
+              projectrollen opnemen. Tegelijkertijd zorgen wij voor een
+              kennisoverdracht binnen je team zodat toekomstige projecten
+              zelfstandig uitgevoerd kunnen worden.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/*------------ SPEAKER ------------ */}
+      <div className={styles.SpeakerContainer}>
+        <div className={styles.SpeakerRightContainer}>
+          <div className={styles.SpeakerRightContent}>
+            <StaticImage
+              src="../images/services/speaker.png"
+              alt="Speaker"
+              width={300}
+              className={styles.imageSpeaker}
+            ></StaticImage>
+          </div>
+        </div>
+        <div className={styles.SpeakerLeftContainer}>
+          <div className={styles.SpeakerLeftContent}>
+            <h4 className={styles.serviceName}>Speaker</h4>
+            <p>
+              Op zoek naar een spreker of facilitator voor je evenement of
+              workshop ? Wij helpen je graag verder.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/*------------ VISUALS ------------ */}
+      <div className={styles.VisualsContainer}>
+        <div className={styles.VisualsLeftContainer}>
+          <div className={styles.VisualsLeftContent}>
+            <StaticImage
+              src="../images/services/visuals.png"
+              alt="Visuals"
+              width={300}
+              className={styles.imageVisuals}
+            ></StaticImage>
+          </div>
+        </div>
+        <div className={styles.VisualsRightContainer}>
+          <div className={styles.VisualsRightContent}>
+            <h4 className={styles.serviceName}>Visuals</h4>
+            <p>
+              Flows, processen of simpelweg de agendapunten van een meeting
+              duidelijk visualiseren, dat is ons ding.
+            </p>
+          </div>
+        </div>
       </div>
 
+      {/* OLD DESIGN CODE FOR REFERENCE 
       <div className={styles.containerButtons}>
         <button
           className={styles.brainfoodBtn}
@@ -141,7 +259,7 @@ const Services = () => {
             </p>
           </details>
         </button>
-      </div>
+        </div> */}
     </Layout>
   )
 }
